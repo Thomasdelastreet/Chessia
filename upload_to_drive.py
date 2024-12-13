7be27935505313bebf50616b7bd31e2b767b7293
Thomasdelastreet <137086763+Thomasdelastreet@users.noreply.github.com>
2024-12-13 17:14:56 +0100

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,17 +1,22 @@
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2.service_account import Credentials
+import os
+
+# Chemin des credentials JSON (fourni via les secrets GitHub)
+GOOGLE_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
 # Initialiser les credentials
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
-creds = Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
+creds = Credentials.from_service_account_file(GOOGLE_CREDENTIALS, scopes=SCOPES)
 
-# Construire le service Drive
+# Construire le service Google Drive
 drive_service = build('drive', 'v3', credentials=creds)
 
 # Définir le fichier à uploader
-file_metadata = {'name': 'chess_ai.pth'}
-media = MediaFileUpload('chess_ai.pth', mimetype='application/octet-stream')
+FILE_NAME = "chess_ai.pth"
+file_metadata = {'name': FILE_NAME}
+media = MediaFileUpload(FILE_NAME, mimetype='application/octet-stream')
 
 # Uploader sur Google Drive
 file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
